eaeb5a1017dddd1b9a5a89e5bc90ea9b1f27f19c
mishrabhi <asmyselfabhishek00@gmail.com>
2022-03-19 20:45:56 +0530

    added star pattern problem

--- Problems/starpattern.c
+++ Problems/starpattern.c
@@ -15,3 +15,81 @@ Reverse Triangular Star Pattern:
 *
 
   */
+
+
+ #include<stdio.h>
+
+void starPattern(int rows)
+{
+    for(int i = 0; i< rows; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+ int main()
+ {
+     int rows;
+     printf("How many rows you want?\n");
+     scanf("%d", &rows);
+     starPattern(rows);
+     return 0;
+ }
+
+
+ /* Output:
+ How many rows you want?
+ 5
+ *
+ **
+ ***
+ ****
+ *****
+
+ */
+
+
+
+
+
+//Reverse Star Pattern:
+
+#include<stdio.h>
+
+void reverseStarPattern(int rows)
+{
+    for(int i=0; i < rows; i++)
+    {
+        for(int j = 0; j <= rows-i-1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int rows;
+    printf("How many rows you want?\n");
+    scanf("%d", &rows);
+    reverseStarPattern(rows);
+    return 0;
+}
+
+
+/* Output:
+How many rows you want?
+5
+*****
+****
+***
+**
+*
+
+*/
+
